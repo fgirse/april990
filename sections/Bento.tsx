6872d67880e0e49306792b3cffb2a57d2a375615
fgirse <fgirs@bluewin.ch>
2025-10-15 11:07:38 +0200

    commit 22er

--- sections/Bento.tsx
+++ sections/Bento.tsx
@@ -9,27 +9,27 @@ export default function Example() {
   return (
     <div className="bento-grid">
       <div className="grid-item-overlay-1760057571577">
-        <h1 className="headingA text-yellow-500 lg:text-9xl">Gastlichkeit ist unsere Philosophie</h1>
+        <h1 className="text-center text-6xl headingA text-yellow-500 lg:text-9xl">Gastlichkeit ist unsere Philosophie</h1>
       </div>
 
       <div className="grid-item-overlay-1760057632942">
-        <div className="mt-[3vh] flex flex-col items-center space-x-4 justify-center h-[70vh] w-[9]">
+        <div className="mt-[3vh] flex flex-col items-center space-x-4 justify-center h-[70vh] w-[]">
           <HeroImage />
         </div>
+         <h1 className="mt-[5vh] text-6xl text-center headingA text-yellow-500 lg:text-7xl">Flens vom Fass</h1>
+
+        <h2 className="text-4xl text-center text-white lg:text-4xl mt-[3vh]">
+          Flensburger Pils - das kühle Blonde von der Waterkant. Astra-Pils - das Kultbier natürlich direkt vom Kiez
+        </h2>
       </div>
 
       <div className="grid-item-overlay-1760057666155">
-        <div className="mt-[3vh] flex flex-col items-center space-x-4 justify-center h-[70vh] w-[9]">
-          <Image src={LogoNeu} alt="LogoNeu" width={500} height={200} />
-        </div>
+        <Image src="/logoNeu.png" alt="CocktailIcon" width={500} height={200} className="mt-[5vh]" />
       </div>
 
       <div className="grid-item-overlay-1760057846834">
         <div className="mt-[3vh] flex flex-col items-center space-x-4 justify-center">
-          <h1 className="headingA text-yellow-500 lg:text-7xl">Flens vom Fass</h1>
-          <h2 className="text-white lg:text-4xl mt-[3vh]">
-            Flensburger Pils - das kühle Blonde von der Waterkant. Astra-Pils - das Kultbier natürlich direkt vom Kiez
-          </h2>
+        
         </div>
       </div>
 
@@ -38,8 +38,8 @@ export default function Example() {
           <Image src="/bottles03.png" alt="WineIcon" width={500} height={200} />
         </div>
         <div className="mt-[5vh] flex flex-col items-center space-x-4 justify-center">
-          <h1 className="text-center headingA text-yellow-500 lg:text-7xl">regionale Weine</h1>
-          <h2 className="text-center text-white lg:text-4xl mt-[3vh]">
+          <h1 className="text-6xl text-center headingA text-yellow-500 lg:text-7xl">regionale Weine</h1>
+          <h2 className="text-center text-4xl text-white lg:text-4xl mt-[3vh]">
             Qualitativ hochwertige Weine aus der Region Kaiserstuhl und dem Markgräflerland. Hauslieferant Weingut Heinemann Scherzingen
           </h2>
         </div>
@@ -49,12 +49,50 @@ export default function Example() {
        
       </div>
       <div className="grid-item-overlay-1760057891398"></div>
-      <div className="grid-item-overlay-1760057918272"></div>
+      <div className="grid-item-overlay-1760057918272">
+          <div className="mt-[5vh] flex flex-col items-center space-x-4 justify-center">
+          <Image src="/cocktailglas.svg" alt="CocktailIcon" width={175} height={175} />
+        </div>
+        <h1 className="mt-[5vh] text-6xl text-center headingA text-yellow-500 lg:text-7xl">Cocktails & Longdrinks</h1>
+        <h2 className="text-4xl text-center text-white lg:text-4xl mt-[3vh]">
+          Erleben Sie unsere kreativen Cocktails und erfrischenden Longdrinks, die mit hochwertigen Spirituosen und frischen Zutaten zubereitet werden.
+        </h2>
+        
+      </div>
       <div className="grid-item-overlay-1760057942145"></div>
-      <div className="grid-item-overlay-1760057948843"></div>
-      <div className="grid-item-overlay-1760057999319"></div>
-      <div className="grid-item-overlay-1760058011364"></div>
-      <div className="grid-item-overlay-1760058017913"></div>
+      <div className="grid-item-overlay-1760057948843">
+        <div className="mt-[5vh] flex flex-col items-center space-x-4 justify-center">
+          <Image src="/fussball.svg" alt="Fussball-Illustration" width={175} height={175} />
+        </div>
+        <h1 className="mt-[5vh] text-6xl text-center headingA text-yellow-500 lg:text-7xl">Fussball</h1>
+        <h1 className="text-center text-6xl headingA text-yellow-500 lg:text-7xl">TV Live</h1>
+        <h2 className="text-4xl text-center text-white lg:text-4xl mt-[3vh]">
+        jeden Samstag-Spieltag der laufenden Bundesliga-Saison Live TV Event in unserer Sportarena natürlich mit Schwerpunkt unseres SC Freiburgs. Wann immer möglich auch Spiele der Champions League und natürlich der grossen Turniere von EM und WM. Bei Topspielen des SC Freiburg mit grosser Publikumsnachfrage sind Reservierungen über unser Booking-Tool zu empfehlen - Unten folgender Button und Du bist direkt dabei !
+        </h2>
+        
+      
+      </div>
+      <div className="grid-item-overlay-1760057999319">
+             <div className="mt-[5vh] flex flex-col items-center space-x-4 justify-center">
+          <Image src="/Crowdparty.png" alt="CocktailIcon" width={600} height={200} />
+        </div>
+        <h1 className="text-6xl mt-[5vh] text-center headingA text-yellow-500 lg:text-7xl">Event</h1>
+        <h2 className="text-4xl text-center text-white lg:text-4xl mt-[3vh]">
+        Privat oder Business-Event? Der perfekte Ort für Ihre Veranstaltung.
+        </h2>
+      </div>
+      <div className="grid-item-overlay-1760058011364">
+        
+      </div>
+      <div className="grid-item-overlay-1760058017913">
+             <div className="mt-[5vh] flex flex-col items-center space-x-4 justify-center">
+          <Image src="/Albers-white.png" alt="CocktailIcon" width={175} height={175} />
+        </div>
+        <h1 className="mt-[5vh] text-6xl text-center headingA text-yellow-500 lg:text-7xl">Hans Albers</h1>
+        <h2 className="text-4xl text-center text-white lg:text-4xl mt-[3vh]">
+          Erleben Sie unsere kreativen Cocktails und erfrischenden Longdrinks, die mit hochwertigen Spirituosen und frischen Zutaten zubereitet werden.
+        </h2>
+      </div>
       <div className="grid-item-overlay-1760058026843"></div>
       <div className="grid-item-overlay-1760058050498"></div>
       <div className="grid-item-overlay-1760058061196"></div>
